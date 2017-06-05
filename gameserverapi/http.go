70d1b9fc2e64d95adba75fe3bf27d32fc8f52dec
Danil Kutkevich <danil@kutkevich.org>
2017-06-05 14:21:47 +0300

    minor

--- gameserverapi/http.go
+++ gameserverapi/http.go
@@ -8,71 +8,71 @@ import (
 	"fmt"
 )
 
-func ResponsePlain(rw http.ResponseWriter, req *http.Request, r io.Reader) {
-	rw.Header().Set("Content-type", "text/plain; charset=utf-8")
+func ResponsePlain(w http.ResponseWriter, r *http.Request, rd io.Reader) {
+	w.Header().Set("Content-type", "text/plain; charset=utf-8")
 	b := new(bytes.Buffer)
 
-	_, err := b.ReadFrom(r)
+	_, err := b.ReadFrom(rd)
 	if err != nil {
 		panic(fmt.Errorf("bytes.(*Buffer).ReadFrom fn error: %s", err.Error()))
 	}
 
-	fmt.Fprint(rw, b.String())
+	fmt.Fprint(w, b.String())
 }
 
-func ResponseXML(rw http.ResponseWriter, req *http.Request, r io.Reader) {
-	rw.Header().Set("Content-type", "application/xml; charset=utf-8")
+func ResponseXML(w http.ResponseWriter, r *http.Request, rd io.Reader) {
+	w.Header().Set("Content-type", "application/xml; charset=utf-8")
 
 	b := new(bytes.Buffer)
 
-	_, err := b.ReadFrom(r)
+	_, err := b.ReadFrom(rd)
 	if err != nil {
 		panic(fmt.Errorf("bytes.(*Buffer).ReadFrom fn error: %s", err.Error()))
 	}
 
-	fmt.Fprint(rw, b.String())
+	fmt.Fprint(w, b.String())
 }
 
 func ResponseJSON(
-	rw http.ResponseWriter,
-	req *http.Request,
+	w http.ResponseWriter,
+	r *http.Request,
 	isSuccess bool,
 	responsePayload interface{},
 	keyValues KV,
 	errs ...*ErrorJSON) {
-	b, err := jsonWithDebug(req.Context(), isSuccess, responsePayload, nil)
+	b, err := jsonWithDebug(r.Context(), isSuccess, responsePayload, nil)
 	if err != nil {
 		panic(fmt.Errorf("answer.jsonWithDebug fn error: %s", err.Error()))
 	}
 
-	responseRawJSON(rw, req, b)
+	responseRawJSON(w, r, b)
 }
 
 func ResponseJSONWithDebug(
-	rw http.ResponseWriter,
-	req *http.Request,
+	w http.ResponseWriter,
+	r *http.Request,
 	isSuccess bool,
 	responsePayload interface{},
 	keyValues KV,
 	errs ...*ErrorJSON) {
 	b, err := jsonWithDebug(
-		req.Context(), isSuccess, responsePayload, keyValues, errs...)
+		r.Context(), isSuccess, responsePayload, keyValues, errs...)
 	if err != nil {
 		panic(fmt.Errorf("answer.jsonWithDebug fn error: %s", err.Error()))
 	}
 
-	responseRawJSON(rw, req, b)
+	responseRawJSON(w, r, b)
 }
 
-func responseRawJSON(rw http.ResponseWriter, req *http.Request, r io.Reader) {
-	rw.Header().Set("Content-type", "application/json; charset=utf-8")
+func responseRawJSON(w http.ResponseWriter, r *http.Request, rd io.Reader) {
+	w.Header().Set("Content-type", "application/json; charset=utf-8")
 
 	b := new(bytes.Buffer)
 
-	_, err := b.ReadFrom(r)
+	_, err := b.ReadFrom(rd)
 	if err != nil {
 		panic(fmt.Errorf("bytes.(*Buffer).ReadFrom fn error: %s", err.Error()))
 	}
 
-	fmt.Fprint(rw, b.String())
+	fmt.Fprint(w, b.String())
 }
