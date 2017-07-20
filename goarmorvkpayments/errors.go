3181123c92b08b2a868f31f4499e52130047e809
Danil Kutkevich <danil@kutkevich.org>
2017-07-20 11:35:14 +0300

    feat: goarmorvkpayments: add vk errors

---
+++ goarmorvkpayments/errors.go
@@ -0,0 +1,36 @@
+package goarmorvkpayments
+
+import "errors"
+
+// <https://vk.com/dev/payments_errors>
+var (
+	ErrDBTemporaryError = JSONError{
+		Code:     2,
+		Critical: false,
+		Err:      errors.New("temporary database error")}
+
+	ErrSignatureMismatc = JSONError{
+		Code:     10,
+		Critical: true,
+		Err:      errors.New("mismatching of the calculated and transmitted signature")}
+
+	ErrQueryParameters = JSONError{
+		Code:     11,
+		Critical: true,
+		Err:      errors.New("query parameters do not meet the specification")}
+
+	ErrProductNotExist = JSONError{
+		Code:     20,
+		Critical: true,
+		Err:      errors.New("product does not exist")}
+
+	ErrProductOutOfStock = JSONError{
+		Code:     21,
+		Critical: true,
+		Err:      errors.New("product is out of stock")}
+
+	ErrUserNotExist = JSONError{
+		Code:     22,
+		Critical: true,
+		Err:      errors.New("user does not exist")}
+)
