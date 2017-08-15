8c0de4f2168409fa38f7eb323f7bc1642269eede
Danil Kutkevich <danil@kutkevich.org>
2017-08-15 11:30:52 +0300

    minor

--- goarmorcachekeys/key_types.go
+++ goarmorcachekeys/key_types.go
@@ -10,8 +10,8 @@ type KeyType int
 func (t KeyType) Sprint(a ...interface{}) string {
 	var b bytes.Buffer
 
-	for _, i := range a {
-		b.WriteString(fmt.Sprint(i))
+	for _, v := range a {
+		b.WriteString(fmt.Sprint(v))
 	}
 
 	return fmt.Sprintf("%d%v", t, b.String())
